dbb4dccb73cd09a1303d82fca55e85fc2f76b948
maryse <gwizamaryse@gmail.com>
2018-10-11 14:54:43 +0900

    First Commit

---
+++ src/components/home.vue
@@ -0,0 +1,209 @@
+<template>
+<div id = "app">
+ <v-app id="light">
+   <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+                   <!-- Yellow top block -->
+
+
+
+
+    <v-flex xs12>
+
+   <v-card tile class="yellow black--text" > 
+
+      <v-card-text  > 
+        <h1 class="text-sm-left"   >Bongo Faster</h1>
+        <p class="text-sm-left" >
+         UDART Mobile App: Mwendokasi on your hands
+        </p>
+      </v-card-text>
+
+      <v-flex  offset-sm8 md2>
+          <v-img src="/lib/img/hand-1.png"> 
+        </v-img>
+      </v-flex>  
+    </v-card>
+
+    </v-flex>
+
+
+    
+
+<!-- second block -->
+            <v-flex xs6 >
+              <v-card class="elevation-0 transparent"
+              img ="/lib/img/top-pht-1.jpg"
+              tile>
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Realtime UDART Info.</div>
+                </v-card-title>
+                <v-card-text>
+                  know the route status </br>
+                  Get bus schedule </br>
+                  Your nearest Station
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs6 >
+              <v-card 
+              color=green 
+              class="elevation-0 transparent"
+              tile >
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Get Route Information,</br> Know station locations, </br> Get Bus Location, Plan Your Trip</div>
+                </v-card-title>
+                <v-card-text  >
+                  No reason to miss the bus </br>
+                  No reason to be late
+                </v-card-text>
+              </v-card>
+            </v-flex>
+
+        <!-- 3rd block Give feedback, new updates-->
+
+        <v-flex xs6 >
+              <v-card color=purple class="elevation-0 transparent"
+              img src="/lib/img/top-pht-1.jpg">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Give Feedback</div>
+                </v-card-title>
+                <v-card-text>
+                  we aim at satisfying our customers </br>
+                  Hearing from our customers, gives courage for improvement </br>
+                  UDART gives you reliable transport at reasonable cost </br>
+                  Tell us your suggestions so that we remain reliable
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs6 >
+              <v-card 
+              class="elevation-0 transparent"
+              tile >
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Get News Updates, Safety tips, guides and share </br> Know station locations, </br> Get Bus Location, Plan Your Trip</div>
+                </v-card-title>
+                <v-card-text  >
+                  Get notification from UDART on time </br>
+                  Any changes on routes schedule, new routes information
+                </v-card-text>
+              </v-card>
+            </v-flex>
+
+
+            <!-- 4th block, how to use bongo Faster -->
+
+            <v-flex xs12 >
+              <v-card 
+              class="elevation-0 transparent"
+              tile >
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">How to Use Bongofaster </div>
+                </v-card-title>
+                <v-card-text  >
+                  Simple | Multilingual | Faster 
+                </v-card-text>
+              </v-card>
+            </v-flex>
+
+
+            <!-- 5th block Current Location, search station, time table-->
+
+        <v-flex xs4 >
+              <v-card tile color=yellow class="elevation-0 transparent"
+              img src="/lib/img/top-pht-1.jpg">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Knowing your current location, </br> Makes you enjoy all features</div>
+                </v-card-title>
+                <v-card-text>
+                  
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs4 >
+              <v-card
+              color=green
+              class="elevation-0 transparent"
+              tile >
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Search your stations accordingly</div>
+                </v-card-title>
+                <v-card-text  >
+                  
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs4 >
+              <v-card 
+              color=blue lighten-2
+              class="elevation-0 transparent"
+              tile >
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Check the time table</div>
+                </v-card-title>
+                <v-card-text  >
+                  
+                </v-card-text>
+              </v-card>
+            </v-flex>
+
+
+
+            <!-- Last block, footer -->
+
+      <v-flex xs12 >
+          <v-footer height="auto">
+            <v-layout justify-center row wrap>
+               <v-btn v-for="link in links"  :key="link" flat round >
+                   {{ link }}
+                </v-btn>
+                <v-flex py-3 text-xs-center xs12>
+                    Copyright &copy;BongoFaster. All rights reserved.
+                 </v-flex>
+             </v-layout>
+          </v-footer>
+        </v-flex>
+
+          
+        
+  
+
+      </v-layout>
+    </v-container>
+   </v-app>
+</div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'About|',
+        'Blog |',
+        'Help |',
+        'Language |',
+        'News |',
+        'Support |',
+        'Parterns |',
+        'terms of Services |',
+        'Privacy Policy|'
+      ]
+    })
+  }
+</script>
+
+<style>
+table.table tbody td{
+	padding: 12px !important;
+}
+.canbe--icon.app-store{
+	background-image: url('/lib/img/it/r2-app-store.png');
+	width: 170px;
+	height: 51px;
+}
+.canbe--icon.google-play{
+	background-image: url('/lib/img/it/r2-google-play.png');
+	width: 170px;
+	height: 56px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index.js'
+
+import Vuetify from 'vuetify'
+
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
